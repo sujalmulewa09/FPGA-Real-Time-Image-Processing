26f6d1435a9820593aa0d7ea71b84e68f5790c8e
sujalmulewa09 <sujalmulewa09@gmail.com>
2026-06-05 11:02:06 +0530

    Update mem_bram.v

--- mem_bram.v
+++ mem_bram.v
@@ -1,31 +1,21 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-/*
- *  Infers a dual-port BRAM with variable width and depth 
- *  
- *  NOTE: 
- *  - One clock delay with read/write
- *
- */
-
 module mem_bram
-#(parameter WIDTH = 11,//top module me ise 12 kar diya hai
+#(parameter WIDTH = 11,
     parameter DEPTH = 640*480)
     (   input wire                      i_wclk,
-        input wire                      i_wr,//hamesha 1 hai
-        input wire [$clog2(DEPTH)-1:0]  i_wr_addr,//this is also decided by the camera capture itself where to send in the b ram
+        input wire                      i_wr,
+        input wire [$clog2(DEPTH)-1:0]  i_wr_addr,
         
-        input wire                      i_rclk,//this is comming from the clock_out1 in the clk_wiz
-        input wire                      i_rd,//always 1 mtlb hmesha is code ko read hi karna hai
-        input wire [$clog2(DEPTH)-1:0]  i_rd_addr,//ye vga top module se aa rha hai .pointing to exactly one of the 307,200 pixels stored in your BRAM.ye input adress pe jo bhi data hoga vo vga top ko milega 
+        input wire                      i_rclk,
+        input wire                      i_rd,
+        input wire [$clog2(DEPTH)-1:0]  i_rd_addr,
         
-        input wire                      i_bram_en,//this is always 1
-        input wire [WIDTH-1:0]          i_bram_data,//ye camera capture se aa rha hai
-        output reg [WIDTH-1:0]          o_bram_data  //ye sidha vga top ko milega taki vo display kar sake   
+        input wire                      i_bram_en,
+        input wire [WIDTH-1:0]          i_bram_data,
+        output reg [WIDTH-1:0]          o_bram_data   
     );
-    
-    // Infer dual-port BRAM with dual clocks
     
     reg [WIDTH-1:0] ram [0:DEPTH-1]; 
     
@@ -35,8 +25,7 @@ module mem_bram
             ram[i_wr_addr] <= i_bram_data;
     
     always @(posedge i_rclk)
-    if(i_rd)// hamesha hi one hai
+    if(i_rd)
         o_bram_data <= ram[i_rd_addr]; 
 
 endmodule
-
