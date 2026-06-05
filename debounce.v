03f3ddca9d1dc539705c0a3a8873bf8f89417028
sujalmulewa09 <sujalmulewa09@gmail.com>
2026-06-05 11:01:23 +0530

    Update debounce.v

--- debounce.v
+++ debounce.v
@@ -1,17 +1,5 @@
 `timescale 1ns / 1ps
-`default_nettype none//` is the instruction for the compiler net means wire means connection between two and type refers to the datatype none means off
-
-/*
- *  Filters glitchy inputs by ignoring state changes
- *  that occur within less than DELAY clock ticks 
- *
- *  Registers state change, debounced output, after 
- *  DELAY number of clock ticks of an unchanged input
- *  
- *  NOTE:
- *  DELAY = (Debounce Time [s]) * (i_clk [Hz])
- * 
- */
+`default_nettype none
 
 module debouncer 
 #(parameter DELAY = 1_000_000)
@@ -23,7 +11,7 @@ module debouncer
 
     localparam MAX_COUNT = $clog2(DELAY);
     reg [MAX_COUNT-1:0] counter;
-    reg                 r_sample; // 
+    reg                 r_sample; 
     
     initial { counter, r_sample } = 0; 
     
@@ -40,7 +28,6 @@ module debouncer
                 counter <= 0;  
         end 
 
-assign o_btn_db = r_sample;//A Fast Human Tap: Roughly 50 ms to 100 ms 
+assign o_btn_db = r_sample;
 
 endmodule
-
